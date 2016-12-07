641b7577700db93b1386b227671583bce62b080d
Pat Putnam <prp2986@rit.edu>
2016-12-06 22:10:48 -0500

    Update available layers

--- javascripts/main.js
+++ javascripts/main.js
@@ -40,34 +40,37 @@ function updateArticles() {
 }
 
 var
+carto = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+}),
 terrain =  L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abcd',
-    minZoom: 0,
-    maxZoom: 20,
     ext: 'png'
 }),
 toner =  L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abcd',
-    minZoom: 0,
-    maxZoom: 20,
     ext: 'png'
+}),
+osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }),
 map = L.map('map', {
     center: [43.0861056, -77.672703],
-    zoom: 15,
-    layers: [terrain, toner]
+    zoom: 12,
+    minZoom: 8,
+    maxZoom: 15,
+    zoomControl: false,
 }),
 baseMaps = {
+    "CartoDB": carto,
+    "OpenStreetMap": osm,
     "Terrian": terrain,
     "Toner": toner
-},
-infoLayers = {
-    "Articles": articleLayer
 };
 
-var layerControl = L.control.layers(baseMaps, infoLayers).addTo(map);
+L.control.layers(baseMaps).addTo(map);
 
 map.on('moveend', function(event) {
     updateArticles();
