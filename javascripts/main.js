d1823db669856f828ae0d295dc49f96f55f71600
Pat Putnam <prp2986@rit.edu>
2016-12-07 23:46:25 -0500

    Treat L.Control.Header as a plugin

--- javascripts/main.js
+++ javascripts/main.js
@@ -252,38 +252,6 @@ sidebar.on('hidden', function(event) {
     }, 250);
 });
 
-L.Control.Header = L.Control.extend({
-
-    initialize: function(template) {
-        var
-        content = this._content = L.DomUtil.get(template),
-        container = this._container = L.DomUtil.create('div', 'leaflet-header');
-
-        content.parentNode.removeChild(content);
-        L.DomUtil.addClass(content, 'leaflet-control');
-
-        container.appendChild(content);
-    },
-
-    addTo: function(map) {
-        var
-        container = this._container,
-        content = this._content;
-
-        var controlContainer = map._controlContainer;
-        controlContainer.insertBefore(container, controlContainer.firstChild);
-
-        this._map = map;
-
-        return this;
-    }
-
-});
-
-L.control.header = function(template) {
-    return new L.Control.Header(template);
-}
-
 var header = L.control.header('header').addTo(map);
 
 updateArticles();
